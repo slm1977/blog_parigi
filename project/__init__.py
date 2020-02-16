8c5d022edcfbaa2a6e691a71366c279f8b39494a
smonni <stefano.monni@crs4.it>
2020-02-16 13:03:09 +0100

    added main blueprint

---
+++ project/__init__.py
@@ -0,0 +1,55 @@
+# tutorial: https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login
+
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+from werkzeug.utils import secure_filename
+
+
+
+
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy()
+
+def create_app():
+    print("Richiamata create_app")
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    UPLOAD_FOLDER = 'static/uploaded_images'
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+    UPLOAD_FOLDER = 'static/uploaded_images'
+
+    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+
+    # blueprint for auth routes in our app
+    #from .auth import auth as auth_blueprint
+    #app.register_blueprint(auth_blueprint)
+
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    return app
+
+
+
---
+++ project/main.py
@@ -0,0 +1,158 @@
+from flask import Flask,  Blueprint, render_template, request, redirect, url_for, send_from_directory
+
+import os
+
+from .ristoranti import ristoranti
+from .itinerari import itinerari
+from .testi import pagine
+
+
+main = Blueprint('main', __name__,template_folder='templates',static_folder='static')
+
+#https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
+#https://stackoverflow.com/questions/54733662/how-to-send-html-data-to-flask-without-form
+
+
+@main.route("/itinerari/<int:indice>/")
+def caricaItinerario(indice):
+    return render_template("blog_itinerario.html", itinerario=itinerari[indice])
+
+
+@main.route("/itinerari/")
+def mostraItinerari():
+    return render_template("blog_itinerari.html", itinerari=itinerari)
+
+
+
+
+@main.route("/")
+def presentazione():
+    #return render_template("home.html")
+    return render_template("blog_content.html", pagina=pagine["presentazione"])
+
+
+@main.route("/aeroporti/")
+def aeroporti():
+    return render_template("blog_content.html", pagina=pagine["aeroporti"])
+
+
+@main.route("/come_muoversi/")
+def come_muoversi():
+    return render_template("blog_content.html", pagina=pagine["come_muoversi"])
+
+
+@main.route("/inner_book/<quartiere>/")
+def caricaLibroRistorante(quartiere):
+    return render_template("blog_restaurant.html", quartiere=quartiere)
+
+@main.route("/ristoranti/")
+def mostra_ristoranti():
+    # (900,550) dimensione del book al 100%
+    ord_ristoranti = list(ristoranti.keys())
+    ord_ristoranti.sort()
+    return render_template("blog_restaurants.html", ristoranti= ord_ristoranti)
+    #return book("marais")
+
+
+
+@main.route("/book/<quartiere>/")
+def book(quartiere):
+    print("In book:%s" % quartiere);
+    myvideo = url_for("static", filename="fotoblog/ristoranti/levieuxbelleville.mp4")
+    return render_template("book2.html", ristoranti=ristoranti[quartiere], countRest=len(ristoranti[quartiere]),
+                           quartiere=quartiere, myvideo=myvideo)
+
+
+@main.route("/ristoranti/<quartiere>/<int:n>/")
+def restaurants(quartiere,n):
+    r = ristoranti[quartiere]
+    print("Ristoranti del %s\n\n" % quartiere)
+    if (n==0):
+        return render_template('rest_page_front_title.html', rist=r[0])
+
+    #elif (n==len(r)-1 and n%2==1):
+    #    return render_template('rest_page_back.html', rist=r[n-1])
+
+    elif (n%2==1):
+        return render_template('rest_page_left.html', rist=r[int((n - 1) / 2)])
+    else:
+        return render_template('rest_page_right_images.html', rist=r[int((n-2) / 2)])
+
+
+
+@main.route("/collage/")
+def collage():
+    return redirect(url_for("static", filename="collage/index.html"))
+
+
+
+@main.route("/video/<quartiere>/<int:n>/")
+def video(quartiere,n):
+    r = ristoranti[quartiere]
+    myvideo = url_for("static", filename="fotoblog/ristoranti/levieuxbelleville.mp4")
+    return render_template('rest_page_right.html', rist=r[n], myvideo=myvideo)
+
+
+@main.route("/editor/")
+def page_editor():
+    #send_from_directory("static", 'itinerari/itinerario_01.html')
+    f = open("static/itinerari/itinerario_01_updated.html", "r")
+    content = f.read()
+    f.close()
+    return render_template("page_editor.html", content=content)
+
+@main.route("/save/", methods=["POST"])
+def save_page():
+    form_data = request.form['content']
+    print("Content:\n%s" % str(form_data))
+    f = open("static/itinerari/itinerario_01_updated.html", "w")
+    f.write(form_data)
+    f.close()
+    return "Pagina salvata"
+
+#
+# FILE UPLOAD CODE
+#
+#https://stackoverflow.com/questions/44926465/upload-image-in-flask
+#https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@main.route("/uploaded/<filename>/")
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+
+@main.route('/upload/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for("main.uploaded_file", filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data action="/upload/">
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
